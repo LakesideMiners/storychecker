9599a74fb71d442f1ec9c9a1aa526fb18ea8053c
LakesideMiners <23389169+LakesideMiners@users.noreply.github.com>
2021-12-15 07:16:04 -0500

    setup for testing GitHub actions

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ def remove_old():
 # Replace the contents of the file
 def replace_words(pattern, replace_with):
     print("Replacing: " + pattern + " with: " + replace_with)
-    with open("input.txt", 'r') as pre:
+    with open("../input.txt", 'r') as pre:
         content = pre.read()
         content = re.sub(pattern, replace_with, content, flags=re.M)
     with open("datafiles/output.txt", 'w') as post:
@@ -45,7 +45,7 @@ def create_diff():
         diff = difflib.HtmlDiff(wrapcolumn=100)  # Create a tool object, set it to wrap then at 80 chars
         result = diff.make_file(content1, content2, context=True, numlines=2)  # Get file comparison results,
         # show the context rather then full file, and shows 2 lines of context
-        diff_file = open("diffoutput.html", "w")
+        diff_file = open("../diffoutput.html", "w")
         diff_file.write(result)  # Output results, you can see the source code written in html)
         diff_file.close()
 
@@ -53,7 +53,7 @@ def create_diff():
 # Do Shit Here
 create_folder()
 remove_old()
-shutil.copy("input.txt", "datafiles/copyfile.txt")
+shutil.copy("../input.txt", "datafiles/copyfile.txt")
 replace_words("(cyn-nick|Cyn-nick|cyn-Nick)", "Cyn-Nick")
 create_diff()
 print("Done")
